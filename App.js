2d62361fd4366d3607e8321704a1853493a71a13
vjspranav <pranavasri@live.in>
2023-06-07 17:08:36 +0530

    Making adding todo beautiful

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
-import { Card, CheckBox } from "react-native-elements";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Card, CheckBox, Header } from "react-native-elements";
 import { useState } from "react";
-import { Button } from "@rneui/base";
+import MyModal from "./components/MyModal";
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -17,23 +17,36 @@ export default function App() {
       isDone: false,
     },
   ]);
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+  const [visible, setVisible] = useState(false);
 
   return (
     <>
+      <Header
+        statusBarProps={{ barStyle: "light-content" }}
+        centerComponent={{
+          text: "Todo App",
+          style: { color: "#fff", fontSize: 20, fontWeight: "bold" },
+        }}
+        containerStyle={{ backgroundColor: "#000" }}
+        // end component add a plus button
+        rightComponent={{
+          icon: "add",
+          color: "#fff",
+          onPress: () => {
+            setVisible(true);
+          },
+        }}
+      />
+      <StatusBar style="hidden" />
       <View style={styles.container}>
-        <Text style={{ fontSize: 30, fontWeight: "bold", marginBottom: 20 }}>
-          Todo App
-        </Text>
         <ScrollView
           width="100%"
           contentContainerStyle={{ alignItems: "center" }}
-          height="40%"
         >
           {todos.length > 0 ? (
             todos.map((todo) => (
               <Card
+                key={todo.title}
                 style={{
                   marginBottom: 10,
                   borderRadius: 5,
@@ -74,7 +87,9 @@ export default function App() {
                       textDecorationLine: todo.isDone ? "line-through" : "none",
                       color: todo.isDone ? "grey" : "black",
                     }}
-                  >{todo.description}</Text>
+                  >
+                    {todo.description}
+                  </Text>
                 </View>
               </Card>
             ))
@@ -82,115 +97,20 @@ export default function App() {
             <Text>No Todos</Text>
           )}
         </ScrollView>
-        {/* 
-        <ScrollView
-          width="50%"
-          contentContainerStyle={{ alignItems: "center" }}
-          height="20%"
-        >
-          {todos.length > 0 ? (
-            todos.map((todo) => (
-              // render normally without cARD
-              <View
-                key={todo.title}
-                style={{
-                  borderWidth: 1,
-                  borderColor: "black",
-                  borderRadius: 5,
-                  padding: 20,
-                  marginBottom: 10,
-                  width: "100%",
-                }}
-              >
-                <Text
-                  style={{
-                    fontSize: 15,
-                    fontWeight: "bold",
-                    marginBottom: 10,
-                  }}
-                >
-                  {todo.title}
-                </Text>
-                <Text
-                  style={{
-                    fontSize: 15,
-                    fontWeight: "bold",
-                    marginBottom: 10,
-                  }}
-                >
-                  {todo.description}
-                </Text>
-              </View>
-            ))
-          ) : (
-            <Text>No Todos</Text>
-          )}
-        </ScrollView> */}
-
-        <Text style={{ fontSize: 30, fontWeight: "bold", marginBottom: 20 }}>
-          Add Todo
-        </Text>
-        <View
-          style={{
-            flexDirection: "column",
-            justifyContent: "space-between",
-            width: "50%",
-            marginTop: 20,
-          }}
-        >
-          <StatusBar style="auto" />
-          <TextInput
-            style={{
-              marginBottom: 10,
-              borderWidth: 1,
-              borderColor: "black",
-              borderRadius: 5,
-              padding: 5,
-              width: "100%",
-            }}
-            placeholder="Title"
-            value={title}
-            onChangeText={(text) => setTitle(text)}
-          />
-          <TextInput
-            style={{
-              marginBottom: 10,
-              borderWidth: 1,
-              borderColor: "black",
-              borderRadius: 5,
-              padding: 5,
-              width: "100%",
-              height: 100,
-            }}
-            placeholder="Description"
-            value={description}
-            onChangeText={(text) => setDescription(text)}
-          />
-          <Button
-            onPress={() => {
-              setTodos([
-                ...todos,
-                {
-                  title: title,
-                  description: description,
-                  isDone: false,
-                },
-              ]);
-              setTitle("");
-              setDescription("");
-            }}
-            title="Add Todo"
-          />
-        </View>
       </View>
+      <MyModal
+        visible={visible}
+        setVisible={setVisible}
+        setTodos={setTodos}
+        todos={todos}
+      />
     </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    marginTop: 200,
+    marginTop: 20,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
---
+++ components/MyModal.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import {
+  Modal,
+  Text,
+  Button,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+const MyModal = ({ visible, setVisible, setTodos, todos }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <Modal
+      visible={visible}
+      animationType="fade"
+      transparent={true}
+      onRequestClose={() => setVisible(false)}
+    >
+      <TouchableOpacity
+        style={{
+          flex: 1,
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        activeOpacity={1}
+        onPress={() => setVisible(false)}
+      >
+        <View
+          style={{
+            backgroundColor: "white",
+            width: 300,
+            height: 300,
+            borderRadius: 10,
+            padding: 20,
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 30, fontWeight: "bold", marginBottom: 20 }}>
+            Add Todo
+          </Text>
+          <TextInput
+            style={{
+              marginBottom: 10,
+              borderWidth: 1,
+              borderColor: "black",
+              borderRadius: 5,
+              padding: 5,
+              width: "100%",
+            }}
+            placeholder="Title"
+            value={title}
+            onChangeText={(text) => setTitle(text)}
+          />
+          <TextInput
+            style={{
+              marginBottom: 10,
+              borderWidth: 1,
+              borderColor: "black",
+              borderRadius: 5,
+              padding: 5,
+              width: "100%",
+              height: 100,
+            }}
+            placeholder="Description"
+            value={description}
+            onChangeText={(text) => setDescription(text)}
+          />
+
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-around",
+              width: "100%",
+            }}
+          >
+            <Button
+              onPress={() => {
+                setTodos([
+                  ...todos,
+                  {
+                    title: title,
+                    description: description,
+                    isDone: false,
+                  },
+                ]);
+                setTitle("");
+                setDescription("");
+                setVisible(false);
+              }}
+              title="Add"
+            />
+            <Button onPress={() => setVisible(false)} title="Cancel" />
+          </View>
+        </View>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            top: 10,
+            right: 10,
+            padding: 10,
+          }}
+          onPress={() => setVisible(false)}
+        >
+          <Text style={{ fontSize: 20 }}>X</Text>
+        </TouchableOpacity>
+      </TouchableOpacity>
+    </Modal>
+  );
+};
+
+export default MyModal;
