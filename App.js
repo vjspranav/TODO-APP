4b9890452e46a0d9ffdd127c29558349c3b8ef62
vjspranav <pranavasri@live.in>
2023-06-07 18:04:14 +0530

    Add a simple filter

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  ToastAndroid,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Card, CheckBox, Header } from "react-native-elements";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
 import MyModal from "./components/MyModal";
 
 export default function App() {
-
-  
   const [todos, setTodos] = useState([]);
   const [visible, setVisible] = useState(false);
+  const [filter, setFilter] = useState("all");
 
   const updateTodos = (todos) => {
     setTodos(todos);
-    AsyncStorage.setItem('todos', JSON.stringify(todos));
+    AsyncStorage.setItem("todos", JSON.stringify(todos));
   };
-  
+
   useEffect(() => {
-    AsyncStorage.getItem('todos').then((value) => {
+    AsyncStorage.getItem("todos").then((value) => {
       if (value) {
         updateTodos(JSON.parse(value));
       }
@@ -41,12 +47,13 @@ export default function App() {
             setVisible(true);
           },
         }}
-        // Add a hamburger menu button to the left which has filters 
+        // Add a hamburger menu button to the left which has filters
         leftComponent={{
           icon: "menu",
           color: "#fff",
           onPress: () => {
-            // Add a menu to filter todos
+            // Toast saying nothing to filter
+            ToastAndroid.show("Nothing yet here Brotha", ToastAndroid.SHORT);
           },
         }}
       />
@@ -62,71 +69,110 @@ export default function App() {
           }}
         >
           <Text>Total: {todos.length}</Text>
-          <Text>
-            Completed: {todos.filter((todo) => todo.isDone).length}
-          </Text>
-          <Text>
-            Remaining: {todos.filter((todo) => !todo.isDone).length}
-          </Text>
+          <Text>Completed: {todos.filter((todo) => todo.isDone).length}</Text>
+          <Text>Remaining: {todos.filter((todo) => !todo.isDone).length}</Text>
         </View>
         <ScrollView
           width="100%"
           contentContainerStyle={{ alignItems: "center" }}
+          height="82%"
         >
           {todos.length > 0 ? (
-            todos.map((todo) => (
-              <Card
-                key={todo.title}
-                style={{
-                  marginBottom: 10,
-                  borderRadius: 5,
-                }}
-              >
-                <Card.Title
+            todos.map((todo) =>
+              filter === "all" ||
+              (filter === "completed" && todo.isDone) ||
+              (filter === "remaining" && !todo.isDone) ? (
+                <Card
+                  key={todo.title}
                   style={{
-                    width: 300,
+                    marginBottom: 10,
+                    borderRadius: 5,
                   }}
                 >
-                  {todo.title}
-                </Card.Title>
-                <Card.Divider />
-                <View
-                  style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                  }}
-                >
-                  <CheckBox
-                    onPress={() => {
-                      updateTodos(
-                        todos.map((item) => {
-                          if (item.title === todo.title) {
-                            return {
-                              ...item,
-                              isDone: !item.isDone,
-                            };
-                          }
-                          return item;
-                        })
-                      );
-                    }}
-                    checked={todo.isDone}
-                  />
-                  <Text
+                  <Card.Title
                     style={{
-                      textDecorationLine: todo.isDone ? "line-through" : "none",
-                      color: todo.isDone ? "grey" : "black",
+                      width: 300,
                     }}
                   >
-                    {todo.description}
-                  </Text>
-                </View>
-              </Card>
-            ))
+                    {todo.title}
+                  </Card.Title>
+                  <Card.Divider />
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                    }}
+                  >
+                    <CheckBox
+                      onPress={() => {
+                        updateTodos(
+                          todos.map((item) => {
+                            if (item.title === todo.title) {
+                              return {
+                                ...item,
+                                isDone: !item.isDone,
+                              };
+                            }
+                            return item;
+                          })
+                        );
+                      }}
+                      checked={todo.isDone}
+                    />
+                    <Text
+                      style={{
+                        textDecorationLine: todo.isDone
+                          ? "line-through"
+                          : "none",
+                        color: todo.isDone ? "grey" : "black",
+                      }}
+                    >
+                      {todo.description}
+                    </Text>
+                  </View>
+                </Card>
+              ) : null
+            )
           ) : (
             <Text>No Todos</Text>
           )}
         </ScrollView>
+
+        <View style={styles.bottomContainer}>
+          <TouchableOpacity
+            onPress={() => {
+              setFilter("all");
+            }}
+            style={{
+              backgroundColor: filter === "all" ? "grey" : "#f1f1f1",
+              ...styles.button,
+            }}
+          >
+            <Text style={styles.buttonText}>All</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: filter === "completed" ? "grey" : "#f1f1f1",
+              ...styles.button,
+            }}
+            onPress={() => {
+              setFilter("completed");
+            }}
+          >
+            <Text style={styles.buttonText}>Completed</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: filter === "remaining" ? "grey" : "#f1f1f1",
+              ...styles.button,
+            }}
+            onPress={() => {
+              setFilter("remaining");
+            }}
+          >
+            <Text style={styles.buttonText}>Remaining</Text>
+          </TouchableOpacity>
+        </View>
       </View>
       <MyModal
         visible={visible}
@@ -144,5 +190,26 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  bottomContainer: {
+    width: "100%",
+    height: "6%",
+    paddingHorizontal: 10,
+    marginTop: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#f1f1f1",
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#000",
   },
 });
